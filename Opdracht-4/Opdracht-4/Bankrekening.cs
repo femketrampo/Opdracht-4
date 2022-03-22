a2bce872317309f330c3763226db6c13a415b32c
femketrampo <76632910+femketrampo@users.noreply.github.com>
2022-03-22 09:49:12 +0100

    Add files via upload

---
+++ Opdracht-4/Opdracht-4/Bankrekening.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht_4
+{
+    class Bankrekening
+    {
+        public double Balans;
+        public double Limiet;
+        public string Verrichtingen;
+
+        public Bankrekening()
+        {
+            Balans = 0;
+            Limiet = 0;
+            Verrichtingen = "";
+        }
+    }
+}
